daf847808ff76dac342a7ad399bc031401b343c7
Gaein nidb <mail@gaein.cn>
2022-10-09 08:00:55 +0800

    init project and basic function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <span>AI+Mobile Lab Novel-AI</span>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <MainPage/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    MainPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,194 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="8">
+        <v-container>
+          <v-row class="my-4">
+            <v-card width="100%">
+              <v-card-title>设置</v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-form>
+                    <v-row class="mx-16">
+                      <v-col cols="12">
+                        <v-text-field
+                            v-model="backend.url"
+                            label="后端地址"
+                            required
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row class="mx-16">
+                      <v-col cols="4">
+                        <v-text-field
+                            v-model="backend.payload.width"
+                            :counter="4"
+                            label="宽度"
+                            required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-text-field
+                            v-model="backend.payload.height"
+                            :counter="4"
+                            label="高度"
+                            required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-select
+                            :items="sampler"
+                            v-model="backend.payload.sampler"
+                            label="采样方法"
+                        ></v-select>
+                      </v-col>
+                    </v-row>
+                    <v-row class="mx-16">
+                      <v-col cols="12">
+                        <v-textarea
+                            v-model="backend.payload.prompt"
+                            rows="5"
+                            label="标签"
+                        >
+                        </v-textarea>
+                      </v-col>
+                    </v-row>
+                    <v-row class="mx-16">
+                      <v-col cols="12">
+                        <v-textarea
+                            v-model="backend.payload.uc"
+                            rows="3"
+                            label="负面标签"
+                        >
+                        </v-textarea>
+                      </v-col>
+                    </v-row>
+                    <v-row class="mx-16">
+                      <v-col cols="4">
+                        <v-text-field
+                            v-model="backend.payload.step"
+                            label="step"
+                        >
+                        </v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-text-field
+                            v-model="backend.payload.scale"
+                            label="scale"
+                        >
+                        </v-text-field>
+                      </v-col>
+                      <v-col cols="4">
+                        <v-text-field
+                            v-model="backend.payload.n_samples"
+                            label="张数"
+                        >
+                        </v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-form>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-col cols="4">
+        <v-container>
+          <v-row class="my-4">
+            <v-card width="100%">
+              <v-card-actions>
+                <v-btn color="primary" @click="generate">发起请求</v-btn>
+                <v-btn color="secondary" @click="clearPicture">清空图片</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-row>
+          <v-row class="my-4">
+            <v-card width="100%" height="370px" class="overflow-y-auto">
+              <v-card-title>图像</v-card-title>
+              <v-card-text>
+                <v-img class="my-2" :contain="true" max-width="100%" v-for="(item, key) in images" :src="item"
+                       :key="key"></v-img>
+              </v-card-text>
+            </v-card>
+          </v-row>
+
+          <v-row class="my-4">
+            <v-card width="100%" height="260px" class="overflow-y-auto ">
+              <v-card-title>请求</v-card-title>
+              <v-card-text>
+                <v-card
+                    dark
+                    flat
+                    tile
+                >
+                  <v-window :dark="true">
+                    <v-window-item> {{ JSON.stringify(backend.payload, null, 2) }}</v-window-item>
+                  </v-window>
+                </v-card>
+              </v-card-text>
+            </v-card>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'MainPage',
+
+  data: () => ({
+    backend: {
+      url: 'http://localhost:8000/generate',
+      payload: {
+        prompt: "masterpiece, best quality, 1girl, cute, school uniform, outside",
+        uc: "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry",
+        seed: 114514,
+        n_samples: 1,
+        sampler: "k_euler_ancestral",
+        width: 256,
+        height: 512,
+        scale: 8,
+        step: 22
+      }
+    },
+    sampler: ['plms', 'ddim', 'k_euler', 'k_euler_ancestral', 'k_heun', 'k_dpm_2', 'k_dpm_2_ancestral', 'k_lms'],
+    images: [],
+    requestLock: false,
+  }),
+  methods: {
+    generate: function () {
+      this.requestLock = true;
+
+      this.backend.payload.seed = parseInt(Math.random() * 10000, 10);
+
+      axios.post(this.backend.url, this.backend.payload)
+          .then(response => {
+            if (response['status'] !== 200 || !response['data'] || !response['data']['output']) {
+              // error
+              alert("非200的返回值，可能是出错了");
+            } else {
+              const outputList = response['data']['output'];
+              outputList.forEach(str => {
+                this.images.unshift("data:image/png;base64," + str);
+              });
+
+              console.log(this.images);
+            }
+            this.requestLock = false;
+          })
+          .catch(error => {
+            alert("由于某些原因无法连接后端（通常是由于显存不足），请等待数分钟后端重启。");
+            console.log(error);
+          })
+    },
+    clearPicture: function () {
+      this.images.splice(0, this.images.length);
+    }
+  }
+}
+</script>
